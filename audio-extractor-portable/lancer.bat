16ae1891e2b10ee753f8725834fd7786a9a62cfa
Guillaume Hemery <guillaume.hemery@ensio.eu>
2026-03-13 12:07:20 +0100

    Update lancer.bat

--- audio-extractor-portable/lancer.bat
+++ audio-extractor-portable/lancer.bat
@@ -4,4 +4,4 @@
 :: Double-cliquez sur ce fichier pour démarrer l'application.
 :: Il contourne la politique d'exécution PowerShell sans modifier le système.
 :: =============================================================================
-powershell.exe -ExecutionPolicy Bypass -NoProfile -File "%~dp0audio-extractor.ps1"
+powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Unblock-File '%~dp0audio-extractor.ps1'; & '%~dp0audio-extractor.ps1'"
